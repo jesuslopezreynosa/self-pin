5bdef48644deb22882981e744686f4adc7a4b883
Jesus Lopez Reynosa <jesuslopezreynosa@fastmail.com>
2026-09-05 19:33:14 -0700

    Server - Cleanup

--- server/Models/GroupMember.cs
+++ server/Models/GroupMember.cs
@@ -5,5 +5,4 @@ public class GroupMember
     public Guid Id { get; set; }
     public Guid GroupId { get; set; }
     public int UserId { get; set; }
-    // public bool PendingKeyRotation { get; set; } = false;
 }
